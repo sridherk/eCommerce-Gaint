c949321d69e9156d8c10e4e94533645ad64a174a
sridherk <36465780+sridherk@users.noreply.github.com>
2019-01-21 07:51:39 +0530

    Update on col-3

--- src/components/Custprofile.js
+++ src/components/Custprofile.js
@@ -18,32 +18,32 @@ class Custprofile extends Component {
         
             <form className="ml-md-5 ml-3 mt-md-3 mr-md-5 mr-3 mt-2 border-top">
                 <div className="form-group row mb-0">
-                    <label className="col-sm-2 col-form-label text-right">Name:</label>
-                    <div className="col-sm-10 order-price mt-2">
+                    <label className="col-sm-2 col-3 col-form-label text-right">Name:</label>
+                    <div className="col-sm-10 col-9 order-price mt-2">
                         {this.state.custDetail.name.first} {this.state.custDetail.name.last}
                     </div>
                 </div>
                 <div className="form-group row mt-0 mb-0">
-                    <label className="col-sm-2 col-form-label text-right">Email:</label>
-                    <div className="col-sm-10 mt-2">
+                    <label className="col-sm-2 col-3 col-form-label text-right">Email:</label>
+                    <div className="col-sm-10 col-9 mt-2">
                         {this.state.custDetail.email}
                     </div>
                 </div>
                 <div className="form-group row mt-0 mb-0">
-                    <label className="col-sm-2 col-form-label text-right">Company:</label>
-                    <div className="col-sm-10 mt-2">
+                    <label className="col-sm-2 col-3 col-form-label text-right">Company:</label>
+                    <div className="col-sm-10 col-9 mt-2">
                         {this.state.custDetail.company}
                     </div>
                 </div>
                 <div className="form-group row mt-0 mb-0">
-                    <label className="col-sm-2 col-form-label text-right">Phone:</label>
-                    <div className="col-sm-10 mt-2">
+                    <label className="col-sm-2 col-3 col-form-label text-right">Phone:</label>
+                    <div className="col-sm-10 col-9 mt-2">
                         {this.state.custDetail.phone}
                     </div>
                 </div>
                 <div className="form-group row mt-0 mb-0">
-                    <label className="col-sm-2 col-form-label text-right">Address:</label>
-                    <div className="col-sm-10 mt-2">
+                    <label className="col-sm-2 col-3 col-form-label text-right">Address:</label>
+                    <div className="col-sm-10 col-9 mt-2">
                         {this.state.custDetail.address}
                     </div>
                 </div>
